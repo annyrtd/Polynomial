087c54c7f4babedb08a0dd932aa81a87e0606e16
annyrtd <fyz95@mail.ru>
2015-03-24 00:02:08 +0300

    Functions calculating polynomial and differential values and writing result to file added.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.io.*;
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,15 +11,31 @@ public class Main {
         try {
             Scanner sc = new Scanner(new File(args[0]));
             int N = sc.nextInt();
-            int[] A = new int[N];
-            for (int i = 0; i < N; i++) {
+            int[] A = new int[N + 1];
+            for (int i = 0; i < (N + 1); i++) {
                 A[i] = sc.nextInt();
             }
-            Polynomial polynomial = new Polynomial(A);
+            int x = sc.nextInt();
+            Polynomial polynomial = new Polynomial(A, N);
+            File myFile = new File(args[1]);
+            if (myFile.exists()) {
+                myFile.delete();
+                myFile.createNewFile();
+            }
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1], true)));
+            out.write("The value of polynomial in point x = " + x + ": "
+                    + polynomial.calculatePolynomialValue(x) + ".\n");
+            out.write("The value of polynomial differential in point x = " + x + ": "
+                    + polynomial.calculatePolynomialDiffValue(x) + ".\n");
+            out.flush();
         } catch (FileNotFoundException e) {
             System.out.println("File cannot be found!");
         } catch (InputMismatchException e) {
             System.out.println("Wrong data in the file!");
+        } catch (IOException e) {
+            System.out.println("A mistake while writing file!");
+        } catch (NoSuchElementException e) {
+            System.out.println("No such element in file!");
         }
     }
 
--- src/com/company/Polynomial.java
+++ src/com/company/Polynomial.java
@@ -4,18 +4,45 @@ package com.company;
  * Created by annyrtd on 23.03.15.
  */
 public class Polynomial {
-
+    /** An array with polynomial coefficients. */
     private int[] array;
-    /** Default constructor */
+
+    /** Polynomial degree. */
+    private int N;
+
+    /** Default constructor. */
     public Polynomial () {
     }
 
     /**
      * Constructor with parameters.
      * @param array an array that should be added.
+     * @param n polynomial degree.
      */
-    public Polynomial(int[] array) {
+    public Polynomial(int[] array, int n) {
         this.array = array;
+        N = n;
+    }
+
+    /**
+     * This function calculates value of the polynomial in a point x.
+     * @param x the point in which value should be calculated.
+     * @return the function value.
+     */
+    public int calculatePolynomialValue(int x) {
+        int temp = 0;
+        for  (int i = 0; i <= N; i++) {
+            temp += array[i] * java.lang.Math.pow(x, i);
+        }
+        return temp;
+    }
+
+    public  int calculatePolynomialDiffValue(int x) {
+        int temp = 0;
+        for  (int i = 1; i <= N; i++) {
+            temp += array[i] * i *  java.lang.Math.pow(x, i - 1);
+        }
+        return temp;
     }
 
 }
